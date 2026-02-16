cfc97dfacf4f701514eb2cc0b3b8b832c56f9486
Stoian Yaroslav KB-14 <yar.stoyan@knu.ua>
2026-02-16 14:24:30 +0200

    basic .js validation

--- frontend/app.js
+++ frontend/app.js
@@ -2,6 +2,65 @@ const tableEl = document.getElementById("table");
 
 const items = [];
 
+function clearError(inputId, errorId){
+    document.getElementById(inputId).classList.remove("invalid");
+    document.getElementById(errorId).innerHTML = "";
+}
+
+function clearErrors(){
+    clearError("dateInput", "dateError");
+    clearError("timeSlotSelect", "timeError");
+    clearError("nameInput", "nameError");
+    clearError("commentInput", "commentError");
+    clearError("statusInput", "statusError");
+}
+
+function showError(inputId, errorId, message){
+    document.getElementById(inputId).classList.add("invalid");
+    document.getElementById(errorId).innerHTML = message;
+}
+
+function validate(){
+    clearErrors();
+
+    let isValid = true;
+    const date = document.getElementById("dateInput").value;
+    const currentDate = new Date();
+    const isoDate = currentDate.toISOString().slice(0,10);
+    if(date === ""){
+        showError("dateInput", "dateError", "Оберіть дату чергування.");
+        isValid = false;
+        return isValid;
+    } else if(date < isoDate){
+        showError("dateInput", "dateError", "Введене значення не коректне.");
+        isValid = false;
+        return isValid
+    }
+    if(document.getElementById("timeSlotSelect").value === ""){
+        showError("timeSlotSelect", "timeError", "Оберіть час зі списку.");
+        isValid = false;
+        return isValid;
+    }
+    const name = document.getElementById("nameInput").value.trim();
+    if(name === ""){
+        showError("nameInput", "nameError", "Обов'язкове поле.");
+        isValid = false;
+        return isValid;
+    }
+    const comment = document.getElementById("commentInput").value.trim();
+    if(comment.length > 30 ){
+        showError("commentInput", "commentError", "Максимальна кількість символів: 30");
+        isValid = false;
+        return isValid;
+    }
+    if(document.getElementById("statusInput").value === ""){
+        showError("statusInput", "statusError", "Оберіть статус запису.");
+        isValid = false;
+        return isValid;
+    }
+    return isValid;
+}
+
 function readForm() {
     return {
         date: document.getElementById("dateInput").value,
@@ -47,6 +106,10 @@ btn.addEventListener("click", () => {
 const form = document.getElementById("createForm");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    const isValid = validate();
+    if(isValid !== true){
+        return;
+    }
     const dto = readForm();
     items.push(dto);
     renderTable(items);
